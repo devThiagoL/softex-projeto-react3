a4369709cf9800af0c0917c1ffab08592e060b07
ThiagoL <106887094+devThiagoL@users.noreply.github.com>
2022-11-17 18:35:17 -0300

    Add files via upload

---
+++ src/componente.js
@@ -0,0 +1,17 @@
+//Importar o React
+import React from 'react';
+
+//Criar a classe com o nome Headers
+class Comp extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Este é o título</h3>
+                <p>Este é o conteúdo</p>
+            </div>
+        );
+    }
+}
+
+//Exportar o componente
+export default Comp;
